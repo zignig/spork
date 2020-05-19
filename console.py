88c83d0a47d99cdf0e0b1f096b249d8d0dfd13bc
Simon Kirkby <obeygiantrobot@gmail.com>
2020-05-19 12:27:20 +0800

    async shell test

--- console.py
+++ console.py
@@ -44,17 +44,27 @@ class CharPad(CodeObject):
 class Switch:
     " Construct a jump table for single chars, or integers "
 
-    def __init__(self, window, select):
+    def __init__(self, window, select, default=None):
         self.mapping = {}
         self.labels = LocalLabels()
         self.window = window
         self.select = select  # a register in window
         window.req(["jumpval"])
 
-    def add(self, val, subroutine):
+    def add(self, item):
+        if len(item) != 2:
+            raise FWError()
+        if isinstance(item, list):
+            for i in item:
+                self.add(i, item)
+        val = item[0]
+        subroutine = item[1]
+        # insert the mapping
         if isinstance(val, str):
             val = ord(val)
-        self.mapping[val] = subroutine
+            self.mapping[val] = subroutine
+        elif isinstance(val, int):
+            self.mapping[val] = subroutine
 
     def dump(self):
         ll = self.labels
@@ -65,12 +75,14 @@ class Switch:
             log.critical("{:d} -> {:d} -> {:s}".format(i, j, str(self.mapping[j])))
             data += [
                 Rem("start-" + str(j)),
-                MOVI(w.jumpval, j),
-                CMP(w.jumpval, self.select),
-                BZ("{:04d}{:s}".format(i, ll._postfix)),
-                Rem("end-" + str(j)),
+                [
+                    MOVI(w.jumpval, j),
+                    CMP(w.jumpval, self.select),
+                    BZ("{:04d}{:s}".format(i, ll._postfix)),
+                    Rem("end-" + str(j)),
+                ],
             ]
-        data += [Rem("end of jump table"), ll("cont")]
+        data += [Rem("end of jump table")]
         for i, j in enumerate(self.mapping):
             log.critical(
                 "trace {:d} -> {:d} -> {:s}".format(i, j, str(self.mapping[j]))
@@ -81,16 +93,21 @@ class Switch:
         return data
 
 
-class Console:
-    def __init__(self):
+class Console(SubR):
+    def setup(self):
+        param = []
+        local = []
+        ret = ["status"]
         # Bind the pad into the function
         # create the functions
         self.accept = self.Accept()
+        self.pad = CharPad()
+        self.w.req("choice")
+        self.selector = sel = Switch(self.w, self.w.choice)
+        # char selector
 
     class Accept(SubR):
         " Accept a char and put it into the pad"
-
-        pad = CharPad()
 
         def setup(self):
             self.params = ["char"]
@@ -112,12 +129,12 @@ if __name__ == "__main__":
     s.test = "this is a test"
     w.req(["pad", "value"])
     cs = Switch(w, w.val)
-    cs.add("c", [ll("a")])
-    cs.add("r", [ll("b")])
-    cs.add("s", [ll("c")])
-    cs.add(43, [ll("d")])
-    cs.add(10, [ll("e")])
-    cs.add(13, [ll("f")])
+    cs.add(("c", [ll("a")]))
+    cs.add(("r", [ll("b")]))
+    cs.add(("s", [ll("c")]))
+    cs.add((43, [ll("d")]))
+    cs.add((10, [ll("e")]))
+    cs.add((13, [ll("f")]))
     d = cs.dump()
     print(d)
     r = Instr.assemble(d)
