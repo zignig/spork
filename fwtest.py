329dd5a626aefe7d306e3576a03661a23511880f
Simon Kirkby <obeygiantrobot@gmail.com>
2020-06-08 16:36:52 +0800

    broken console commented out

--- fwtest.py
+++ fwtest.py
@@ -8,7 +8,7 @@ from ideal_spork.peripheral.serial import AsyncSerialPeripheral
 from ideal_spork.peripheral.timer import TimerPeripheral
 from ideal_spork.peripheral.leds import LedPeripheral
 from ideal_spork.peripheral.kermit_crc import KermitCRC
-from ideal_spork.peripheral.warmboot import WarmBoot
+from ideal_spork.peripheral.warmboot import WarmBoot, FakeWarm
 
 from ideal_spork.cores.ext_reset import ExternalReset
 from ideal_spork.cores.debounce import Debounce
@@ -29,9 +29,12 @@ crc_16_kermit = crcmod.predefined.mkPredefinedCrcFun("kermit")
 
 
 class TestSpork(Elaboratable):
-    def __init__(self, platform, uart_speed=9600, mem_size=512, firmware=None):
+    def __init__(
+        self, platform, uart_speed=9600, mem_size=512, firmware=None, sim=False
+    ):
         self.platform = platform
         self.cpu = cpu = BonelessSpork(firmware=firmware, mem_size=mem_size)
+        self.sim = sim
 
         # Request a uart at speed from the platform
         uart = platform.request("uart")
@@ -56,34 +59,35 @@ class TestSpork(Elaboratable):
         crc = KermitCRC()
         cpu.add_peripheral(crc)
 
-        # ice40 warmboot device
-        warm = WarmBoot()
-        cpu.add_peripheral(warm)
+        if sim == False:
+            # ice40 warmboot device
+            warm = WarmBoot()
+            cpu.add_peripheral(warm)
+
+            # Semi external device to reset on an out of band pin
+            # DTR on FTDI, 4 toggles -> warmboot , 7 toggles bootloader
+            # within a timeout.
+
+            # add the external interface for the warmboot
+            # runs off the DTR pin of the FTDI
+            dtr = platform.request("reset_pin")
+            # the warmboot instance
+            wb = warm.warm
+            self.er = ExternalReset(wb.select, wb.ext_image, wb.ext_boot, dtr)
+        else:
+            warm = FakeWarm()
+            cpu.add_peripheral(warm)
 
         # build the register map
         cpu.build()
-
-        # debouncer test
-        # but = platform.request('button')
-        # self.db = Debounce(but)
-
-        # Semi external device to reset on an out of band pin
-        # DTR on FTDI, 4 toggles -> warmboot , 7 toggles bootloader
-        # within a timeout.
-
-        # add the external interface for the warmboot
-        # runs off the DTR pin of the FTDI
-        dtr = platform.request("reset_pin")
-        # the warmboot instance
-        wb = warm.warm
-        self.er = ExternalReset(wb.select, wb.ext_image, wb.ext_boot, dtr)
 
     def elaborate(self, platform):
         m = Module()
         # Attach the cpu
         m.submodules.cpu = self.cpu
         # Attach the external reset
-        m.submodules.external_reset = self.er
+        if not self.sim:
+            m.submodules.external_reset = self.er
         # Attache the debouncer
         return m
 
@@ -92,7 +96,7 @@ from echo_fw import Echo
 from bootloader import Bootloader
 
 
-def build(TheFirmware, mem_size=512):
+def build(TheFirmware, mem_size=512, sim=False):
     # for programming from a firmware file
     print("Testing Spork")
     platform = TinyFPGABXPlatform()
@@ -108,7 +112,7 @@ def build(TheFirmware, mem_size=512):
     )
     # print(platform.resources)
     # Spork it up
-    spork = TestSpork(platform, uart_speed=115200, mem_size=mem_size)
+    spork = TestSpork(platform, uart_speed=115200, mem_size=mem_size, sim=sim)
     # Build the firmware
     print(spork.cpu.map.show())
     f = TheFirmware(spork.cpu.map, start_window=mem_size)
@@ -122,8 +126,6 @@ def build(TheFirmware, mem_size=512):
 
 if __name__ == "__main__":
 
-    spork = build(Bootloader, mem_size=1024)
-
     import argparse
 
     parser = argparse.ArgumentParser()
@@ -133,6 +135,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.simulate:
+        spork = build(Bootloader, mem_size=1024)
         from nmigen.cli import pysim
         from sim_data import test_rx, str_data
 
@@ -147,10 +150,16 @@ if __name__ == "__main__":
             sim.run_until(1000, run_passive=True)
 
     if args.program:
+        spork = build(Bootloader, mem_size=1024)
         spork.platform.build(spork, do_program=True)
 
     if args.generate:
+        spork = build(Bootloader, mem_size=1024, sim=True)
         from nmigen.back import rtlil, verilog, pysim
 
-        output = rtlil.convert(spork, name="cxx.il")
-        print(output)
+        output = rtlil.convert(
+            spork, name="cxx.il", ports=[spork.cpu.pc.devices[0]._phy.tx.o]
+        )
+        f = open("../cxxrtl_sim/cxx.il", "w")
+        f.write(output)
+        f.close()
