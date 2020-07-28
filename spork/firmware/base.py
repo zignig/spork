70c9e06321e144e19329b9af989eacb81aeea2de
Simon Kirkby <obeygiantrobot@gmail.com>
2020-07-28 21:32:49 +0800

    remove duplicate firmware class

--- spork/firmware/base.py
+++ spork/firmware/base.py
@@ -400,6 +400,7 @@ class SubR(metaclass=MetaSub):
         pass
 
     def __call__(self, *args, **kwargs):
+        # TODO this will need to be rewritten for the allocator
         if len(args) != self.length:
             raise ValueError("Parameter count is should be '{}'".format(self.length))
         # load the parameters into the next frame
@@ -449,6 +450,7 @@ class SubR(metaclass=MetaSub):
         return []
 
     def code(self):
+        # TODO This needs to change for the allocator
         data = [L(self.name)]
         if self.__doc__ is not None:
             data.append(Rem(self.__doc__))
@@ -456,85 +458,6 @@ class SubR(metaclass=MetaSub):
             data += [Rem(self.w._name)]
         data += [ADJW(-self._size)]  # window shift up
         data += [LDW(self.w.fp, 0)]  # save window
-        # TODO process this line by line, insert spills above
         data += self.instr()
         data += [ADJW(self._size), JR(R7, 0)]  # shift window down
         return [data]
-
-
-class Firmware:
-    """ 
-    Firmware construct 
-
-    does initialization , main loop and library code
-    """
-
-    def __init__(self, reg=None, start_window=512):
-        log.info("Create Firmware Object")
-        self.w = Window()
-        self.sw = start_window
-        self.reg = reg
-        # attach the io_map to all the subroutines
-        SubR.reg = self.reg
-        Inline.reg = self.reg
-        # code objects
-        self.obj = []
-        self._built = False
-        self.fw = None
-
-    def setup(self):
-        raise FWError("No setup function")
-
-    def prelude(self):
-        log.warning("No prelude(), use to setup code")
-        return []
-
-    def instr(self):
-        return []
-
-    def code(self):
-        if not self._built:
-            w = self.w = Window()
-            ll = LocalLabels()
-            self.setup()
-            fw = [
-                Rem("--- Firmware Object ---"),
-                Rem(self.w._name),
-                L("init"),
-                MOVI(w.fp, self.sw - 8),
-                STW(w.fp),
-                self.prelude(),
-                L("main"),
-                self.instr(),
-                J("main"),
-                Rem("--- Library Code ---"),
-                MetaSub.code(),
-                Rem("--- Data Objects ---"),
-                CodeObject.get_code(),
-                L("program_start"),
-            ]
-            self._built = True
-            self.fw = fw
-        else:
-            fw = self.fw
-        return fw
-
-    def show(self):
-        pprint.pprint(self.code(), width=1, indent=2)
-
-    def assemble(self):
-        fw = Instr.assemble(self.code())
-        return fw
-
-    def hex(self):
-        asm = self.assemble()
-        full_hex = ""
-        for i in asm:
-            hex_string = "{:04X}".format(i)
-            full_hex += hex_string
-        return full_hex
-
-    def disassemble(self):
-        c = self.assemble()
-        a = Assembler()
-        return a.disassemble(c)
