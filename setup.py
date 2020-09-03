bbdcf313de877e4b8782854cf5fb07a2c63756d8
Simon Kirkby <obeygiantrobot@gmail.com>
2020-09-03 08:32:22 +0800

    minimal setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="spork",
+    version="0.1",
+    author="zignig",
+    description="Boneless v3 , and peripherals",
+    packages=find_packages(),
+    project_urls={
+        "Source Code": "https://github.com/zignig/spork",
+        "Bug Tracker": "https://github.com/zignig/spork/issues",
+    },
+    # entry_points={"console_scripts": ["spork = ideal_spork.cli:as_main"]},
+)
