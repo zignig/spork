1fde385da146a6d8e5b77c560604d2d1ef2faf6e
Simon Kirkby <obeygiantrobot@gmail.com>
2022-04-19 16:45:06 +0800

    Updated lsfr to state machine , better

--- spork/peripheral/lsfr.py
+++ spork/peripheral/lsfr.py
@@ -1,7 +1,6 @@
 """
 Linear feedback shift register
 """
-
 from nmigen import *
 
 from functools import reduce
@@ -65,30 +64,34 @@ class LSFR(Peripheral, Elaboratable):
         out = Signal()  # the output bit
         # make all the internal taps and with the data
         m.d.comb += self.match.eq(self.taps_internal & self.value.r_data)
-        # create an XOR monster that binds unused taps
+        # create an XOR monster that binds all taps . zeros propagate
         # I _think_ that it will work out
         m.d.comb += out.eq(~reduce(xor, [self.match[i] for i in range(self.SIZE)]))
 
-        with m.If(self.startup == 0):
-            m.d.sync += self.taps_internal.eq(self.initial_taps)
-            m.d.sync += self.value.r_data.eq(self.initial)
-            m.d.sync += self.current.eq(self.initial)
-            m.d.sync += self.mode.r_data.eq(1)
-            # disable the initialize
-            m.d.sync += self.incr.eq(1)
-            m.d.sync += self.startup.eq(1)
-        with m.Else():
-            with m.If(self.value.r_stb):
-                m.d.sync += self.value.r_data.eq(self.current)
+        with m.FSM() as fsm:
+            with m.State("INIT"):
+                m.d.sync += self.taps_internal.eq(self.initial_taps)
+                m.d.sync += self.value.r_data.eq(self.initial)
+                m.d.sync += self.current.eq(self.initial)
+                m.d.sync += self.mode.r_data.eq(0)
+                # disable the initialize
                 m.d.sync += self.incr.eq(1)
-            with m.If(self.incr == 1):  # increment
+                m.d.sync += self.startup.eq(1)
+                m.next = "INCREMENT"
+            with m.State("INCREMENT"):
+                # advance the lsfr
                 m.d.sync += Cat(self.current).eq(Cat(out, self.current))
+                m.next = "MAYBEWAIT"
+
+            with m.State("MAYBEWAIT"):
+                m.d.sync += self.value.r_data.eq(self.current)
                 with m.If(self.mode.r_data == 1):
-                    m.d.sync += self.incr.eq(1)
+                    # mode = 1 , increment on read
+                    with m.If(self.value.r_stb):
+                        m.next = "INCREMENT"
                 with m.Else():
-                    m.d.sync += self.incr.eq(
-                        0
-                    )  # stop the next update until read strobe
+                    # mode = 0 , free running
+                    m.next = "INCREMENT"
 
         # update the registers with a write
         with m.If(self.taps.w_stb):
@@ -97,7 +100,7 @@ class LSFR(Peripheral, Elaboratable):
         with m.If(self.mode.w_stb):
             m.d.sync += self.mode.r_data.eq(self.mode.w_data)
         with m.If(self.value.w_stb):
-            # m.d.sync += self.value.r_data.eq(self.value.w_data)
+            m.d.sync += self.value.r_data.eq(self.value.w_data)
             m.d.sync += self.current.eq(self.value.w_data)
         return m
 
